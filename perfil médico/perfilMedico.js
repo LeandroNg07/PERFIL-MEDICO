321cd40ac7e64ae289038008c95f1412c462cde4
LeandroNg07 <126211703+LeandroNg07@users.noreply.github.com>
2023-05-24 22:00:35 -0300

    Add files via upload

---
+++ perfil médico/perfilMedico.js
@@ -0,0 +1,11 @@
+// botoes header
+function clicou(obj){
+    const collection = document.getElementsByClassName("navHeader")
+    
+    for(let i = 0; i < collection.length; i++){
+    collection[i].classList.remove("selected");
+    }
+    obj.classList.add("selected");
+    
+    }
+    
